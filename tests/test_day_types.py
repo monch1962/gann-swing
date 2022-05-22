f4f20a8fec6a524b052845747035810df1bb8683
David Mitchell <monch1962@gmail.com>
2022-05-22 13:27:18 +0000

    Added failing swing tests

--- tests/test_day_types.py
+++ tests/test_day_types.py
@@ -4,21 +4,23 @@ import numpy as np
 import pandas as pd
 
 @pytest.fixture
-def setup_bars():
+def gs():
+    '''
+    We're going to set up a set of bars to test each day type
+    '''
     data = [
         [np.nan, np.nan, 5, 1, np.nan],
-        [np.nan, np.nan, 6, 2, np.nan], # bar 1 = Up day
-        [np.nan, np.nan, 5, 1, np.nan], # bar 2 = Down day
-        [np.nan, np.nan, 4, 2, np.nan], # bar 3 = Inside day
-        [np.nan, np.nan, 5, 1, np.nan], # bar 4 = Outside day
-        [np.nan, np.nan, 5, 1, np.nan] # bar 5 = Inside day (same as previous bar)
+        [np.nan, np.nan, 6, 2, np.nan], # bar 1 = Up bar vs previous bar
+        [np.nan, np.nan, 5, 1, np.nan], # bar 2 = Down bar vs previous bar
+        [np.nan, np.nan, 4, 2, np.nan], # bar 3 = Inside bar vs previous bar
+        [np.nan, np.nan, 5, 1, np.nan], # bar 4 = Outside bar vs previous bar
+        [np.nan, np.nan, 5, 1, np.nan] # bar 5 = Inside day (same as previous bar) vs previous bar
     ]
     bars = pd.DataFrame(data, columns=['Timestamp', 'Open', 'High', 'Low', 'Close'])
-    return bars
+    gs = GannSwing(bars=bars)
+    return gs
 
-def test_up_day(setup_bars):
-    gs = GannSwing(bars=setup_bars)
-    print(gs.bars.to_string())
+def test_up_day(gs):
     assert gs._up_day(bar=1)
     assert not gs._up_day(bar=2)
     assert not gs._up_day(bar=3)
@@ -26,24 +28,21 @@ def test_up_day(setup_bars):
     assert not gs._up_day(bar=5)
 
 
-def test_down_day(setup_bars):
-    gs = GannSwing(bars=setup_bars)
+def test_down_day(gs):
     assert not gs._down_day(bar=1)
     assert gs._down_day(bar=2)
     assert not gs._down_day(bar=3)
     assert not gs._down_day(bar=4)
     assert not gs._down_day(bar=5)
 
-def test_inside_day(setup_bars):
-    gs = GannSwing(bars=setup_bars)
+def test_inside_day(gs):
     assert not gs._inside_day(bar=1)
     assert not gs._inside_day(bar=2)
     assert gs._inside_day(bar=3)
     assert not gs._inside_day(bar=4)
     assert gs._inside_day(bar=5)
 
-def test_outside_day(setup_bars):
-    gs = GannSwing(bars=setup_bars)
+def test_outside_day(gs):
     assert not gs._outside_day(bar=1)
     assert not gs._outside_day(bar=2)
     assert not gs._outside_day(bar=3)
---
+++ tests/test_swings.py
@@ -0,0 +1,40 @@
+import pytest
+from gannswing import GannSwing
+import numpy as np
+import pandas as pd
+
+@pytest.fixture
+def gs():
+    '''
+    We're going to set up a set of bars to test each type of swing
+    '''
+    data = [
+        [0, np.nan, 5, 1, np.nan],
+        [1, np.nan, 6, 2, np.nan], # bar 1 = Up bar vs previous bar
+        [2, np.nan, 5, 1, np.nan], # bar 2 = Down bar vs previous bar
+        [3, np.nan, 4, 2, np.nan], # bar 3 = Inside bar vs previous bar
+        [4, np.nan, 5, 1, np.nan], # bar 4 = Outside bar vs previous bar
+        [5, np.nan, 5, 1, np.nan] # bar 5 = Inside day (same as previous bar) vs previous bar
+    ]
+    bars = pd.DataFrame(data, columns=['Timestamp', 'Open', 'High', 'Low', 'Close'])
+    gs = GannSwing(bars=bars)
+    return gs
+
+def test_1_day_swing_timestamps(gs):
+    '''
+    Check that the correct set of 1 day swings are detected
+    '''
+    swings_found = gs.calculate(swing_days=1)
+    correct_swings = [1, 4, 17] # These are the swing timestamps that should be detected
+
+    # First confirm that the number of swings we found is correct
+    assert len(swings_found) == len(correct_swings) 
+
+    # Now check each swing that gets returned
+    for i in range(0, len(swings_found)-1):
+        if i in correct_swings:
+            # We should have a swing returned for this timestamp
+            assert swings.loc[i]['Timestamp'] == i
+        else:
+            # We shouldn't have a swing returned for this timestamp
+            assert swings.loc[i]['Timestamp'] != i
