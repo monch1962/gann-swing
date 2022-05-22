5ceaad698c5aa750a9f7b21e76539a38030b5a31
David Mitchell <monch1962@gmail.com>
2022-05-22 02:45:16 +0000

    Added simple class-level tests

--- gannswing.py
+++ gannswing.py
@@ -2,4 +2,21 @@ import pandas as pd
 import numpy as np
 
 class GannSwing():
-    pass
+    '''
+    Class to perform Gann swing calculations
+    '''
+    def __init__(self, bars, swing_days=1, inside_down=False, ignore_threshold=0, use_close_of_outside_bar=False):
+        '''
+        Parameters:
+        - (required) a pandas dataframe containing OHLC data
+        - (optional) the number of days required to trigger a swing
+        - (optional) should an inside day in a down trend trigger a swing?
+        - (optional) what is a small enough swing to ignore
+        - (optional) use the close of an outside bar to decide the swing direction
+        '''
+        self.swing_days = swing_days
+        self.inside_down = inside_down
+        self.ignore_threshold = ignore_threshold
+
+if __name__ == '__main__':
+    gs = GannSwing(bars=pd.DataFrame())
--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,6 +1,25 @@
 import pytest
 from gannswing import GannSwing
+import pandas as pd
 
-def test_class_exists():
-    gs = GannSwing()
+@pytest.fixture
+def empty_dataframe():
+    return pd.DataFrame()
+
+@pytest.fixture
+def zero_bars():
+    return pd.DataFrame(columns=['Timestamp', 'Open', 'High', 'Low', 'Close'])
+
+def test_class_create_fails_with_no_bars():
+    with pytest.raises(TypeError):
+        gs = GannSwing()
+
+def test_class_create_success_with_bars():
+    bars = pd.DataFrame()
+    gs = GannSwing(bars=empty_dataframe)
+    assert gs != None
+
+def test_swing_days():
+    bars = pd.DataFrame()
+    gs = GannSwing(bars=empty_dataframe, swing_days=3)
     assert gs != None
